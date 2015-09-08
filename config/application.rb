83af011603cf7143097a654922d3ada29af0631f
Paweł Usowski <pawel.usowski@gmail.com>
2015-09-09 00:30:29 +0200

    remove test folder

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Flags
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # surpass generation of tests
+    config.generators.test_framework false
   end
 end
