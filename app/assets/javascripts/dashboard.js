8d5daaff51d55e3240130b63808525a96e2be3f4
Paweł Usowski <pawel.usowski@gmail.com>
2015-10-13 02:39:18 +0200

    Set main map center to Kraków

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,7 +3,7 @@ require('leaflet/dist/leaflet.css')
 
 class Dashboard {
   showMainMap() {
-    var map = L.map('main_map').setView([51.505, -0.09], 13);
+    var map = L.map('main_map').setView([50.06, 19.93], 13);
 
     L.Icon.Default.imagePath = 'webpack/leaflet'
 
@@ -11,9 +11,9 @@ class Dashboard {
         attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-      .openPopup();
+    //L.marker([51.5, -0.09]).addTo(map)
+    //  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //  .openPopup();
   }
 }
 
